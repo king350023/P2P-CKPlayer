802656fcd3b0f377c202040af9c4dd9c570f709b
king350023 <46060641+king350023@users.noreply.github.com>
2021-05-02 01:04:23 +0800

    master

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
-
 const path = require('path');
 
 module.exports = {
-    entry:'./ckplayer/ckplayer.js',
+    entry: './ckplayer/ckplayer.js',
     output: {
         filename: 'ckplayer.min.js',
         path: path.resolve(__dirname, 'ckplayer')
